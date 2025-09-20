7231f3e611d40a5529914e7b9ec98042cdaa20cb
BrahimBenzekri <benzbra04@gmail.com>
2025-09-20 08:27:19 +0100

    Added: Theme Providers

---
+++ lib/presentation/providers/theme_provider.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+part 'theme_provider.g.dart';
+
+enum ThemeOption {
+  light,
+  dark,
+  system;
+
+  String get displayName {
+    switch (this) {
+      case ThemeOption.light:
+        return 'Light';
+      case ThemeOption.dark:
+        return 'Dark';
+      case ThemeOption.system:
+        return 'System';
+    }
+  }
+
+  IconData get icon {
+    switch (this) {
+      case ThemeOption.light:
+        return Icons.light_mode;
+      case ThemeOption.dark:
+        return Icons.dark_mode;
+      case ThemeOption.system:
+        return Icons.brightness_auto;
+    }
+  }
+}
+
+// SharedPreferences provider for dependency injection
+@riverpod
+Future<SharedPreferences> sharedPreferences(ref) async {
+  return await SharedPreferences.getInstance();
+}
+
+// Theme Notifier with code generation
+@riverpod
+class ThemeNotifier extends _$ThemeNotifier {
+  static const String _themeKey = 'theme_mode';
+
+  @override
+  FutureOr<ThemeOption> build() async {
+    // Load saved theme on initialization
+    return await _loadTheme();
+  }
+
+  Future<ThemeOption> _loadTheme() async {
+    try {
+      final prefs = await ref.read(sharedPreferencesProvider.future);
+      final themeIndex = prefs.getInt(_themeKey);
+
+      if (themeIndex != null && themeIndex < ThemeOption.values.length) {
+        return ThemeOption.values[themeIndex];
+      }
+    } catch (e) {
+      // If loading fails, return system default
+      debugPrint('Failed to load theme preference: $e');
+    }
+
+    return ThemeOption.system; // Default fallback
+  }
+
+  Future<void> setTheme(ThemeOption option) async {
+    // Optimistic update
+    state = AsyncValue.data(option);
+
+    try {
+      final prefs = await ref.read(sharedPreferencesProvider.future);
+      await prefs.setInt(_themeKey, option.index);
+    } catch (e) {
+      // Revert on error
+      state = AsyncValue.error(e, StackTrace.current);
+      debugPrint('Failed to save theme preference: $e');
+    }
+  }
+
+  void toggleTheme() {
+    state.whenData((currentTheme) {
+      final nextTheme = switch (currentTheme) {
+        ThemeOption.light => ThemeOption.dark,
+        ThemeOption.dark => ThemeOption.system,
+        ThemeOption.system => ThemeOption.light,
+      };
+      setTheme(nextTheme);
+    });
+  }
+}
+
+// Convert ThemeOption to Flutter's ThemeMode
+@riverpod
+ThemeMode themeMode(ref) {
+  final themeAsync = ref.watch(themeNotifierProvider);
+
+  return themeAsync.when(
+    data:
+        (ThemeOption themeOption) => switch (themeOption) {
+          ThemeOption.light => ThemeMode.light,
+          ThemeOption.dark => ThemeMode.dark,
+          ThemeOption.system => ThemeMode.system,
+        },
+    loading: () => ThemeMode.system, // Default while loading
+    error: (_, __) => ThemeMode.system, // Fallback on error
+  );
+}
+
+// Helper provider to check if current theme is dark
+@riverpod
+bool isDarkMode(ref, BuildContext context) {
+  final ThemeMode themeMode = ref.watch(themeModeProvider);
+
+  return switch (themeMode) {
+    ThemeMode.light => false,
+    ThemeMode.dark => true,
+    ThemeMode.system =>
+      MediaQuery.platformBrightnessOf(context) == Brightness.dark,
+  };
+}
+
+// Provider for getting current theme option safely
+@riverpod
+ThemeOption currentThemeOption(ref) {
+  final themeAsync = ref.watch(themeNotifierProvider);
+  return themeAsync.maybeWhen(
+    data: (theme) => theme,
+    orElse: () => ThemeOption.system,
+  );
+}
---
+++ lib/presentation/providers/theme_provider.g.dart
@@ -0,0 +1,219 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'theme_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$sharedPreferencesHash() => r'a2e33dc9fa8ab78049dab3549e2dfb444921fc4c';
+
+/// See also [sharedPreferences].
+@ProviderFor(sharedPreferences)
+final sharedPreferencesProvider =
+    AutoDisposeFutureProvider<SharedPreferences>.internal(
+      sharedPreferences,
+      name: r'sharedPreferencesProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$sharedPreferencesHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SharedPreferencesRef = AutoDisposeFutureProviderRef<SharedPreferences>;
+String _$themeModeHash() => r'74ff62ae0d5d918c425e46b756ccb8986168a2be';
+
+/// See also [themeMode].
+@ProviderFor(themeMode)
+final themeModeProvider = AutoDisposeProvider<ThemeMode>.internal(
+  themeMode,
+  name: r'themeModeProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$themeModeHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef ThemeModeRef = AutoDisposeProviderRef<ThemeMode>;
+String _$isDarkModeHash() => r'f9dc13ad0cfcc3cafe9ebafdd63e2e471e2b4c84';
+
+/// Copied from Dart SDK
+class _SystemHash {
+  _SystemHash._();
+
+  static int combine(int hash, int value) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + value);
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x0007ffff & hash) << 10));
+    return hash ^ (hash >> 6);
+  }
+
+  static int finish(int hash) {
+    // ignore: parameter_assignments
+    hash = 0x1fffffff & (hash + ((0x03ffffff & hash) << 3));
+    // ignore: parameter_assignments
+    hash = hash ^ (hash >> 11);
+    return 0x1fffffff & (hash + ((0x00003fff & hash) << 15));
+  }
+}
+
+/// See also [isDarkMode].
+@ProviderFor(isDarkMode)
+const isDarkModeProvider = IsDarkModeFamily();
+
+/// See also [isDarkMode].
+class IsDarkModeFamily extends Family<bool> {
+  /// See also [isDarkMode].
+  const IsDarkModeFamily();
+
+  /// See also [isDarkMode].
+  IsDarkModeProvider call(BuildContext context) {
+    return IsDarkModeProvider(context);
+  }
+
+  @override
+  IsDarkModeProvider getProviderOverride(
+    covariant IsDarkModeProvider provider,
+  ) {
+    return call(provider.context);
+  }
+
+  static const Iterable<ProviderOrFamily>? _dependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get dependencies => _dependencies;
+
+  static const Iterable<ProviderOrFamily>? _allTransitiveDependencies = null;
+
+  @override
+  Iterable<ProviderOrFamily>? get allTransitiveDependencies =>
+      _allTransitiveDependencies;
+
+  @override
+  String? get name => r'isDarkModeProvider';
+}
+
+/// See also [isDarkMode].
+class IsDarkModeProvider extends AutoDisposeProvider<bool> {
+  /// See also [isDarkMode].
+  IsDarkModeProvider(BuildContext context)
+    : this._internal(
+        (ref) => isDarkMode(ref as IsDarkModeRef, context),
+        from: isDarkModeProvider,
+        name: r'isDarkModeProvider',
+        debugGetCreateSourceHash:
+            const bool.fromEnvironment('dart.vm.product')
+                ? null
+                : _$isDarkModeHash,
+        dependencies: IsDarkModeFamily._dependencies,
+        allTransitiveDependencies: IsDarkModeFamily._allTransitiveDependencies,
+        context: context,
+      );
+
+  IsDarkModeProvider._internal(
+    super._createNotifier, {
+    required super.name,
+    required super.dependencies,
+    required super.allTransitiveDependencies,
+    required super.debugGetCreateSourceHash,
+    required super.from,
+    required this.context,
+  }) : super.internal();
+
+  final BuildContext context;
+
+  @override
+  Override overrideWith(bool Function(IsDarkModeRef provider) create) {
+    return ProviderOverride(
+      origin: this,
+      override: IsDarkModeProvider._internal(
+        (ref) => create(ref as IsDarkModeRef),
+        from: from,
+        name: null,
+        dependencies: null,
+        allTransitiveDependencies: null,
+        debugGetCreateSourceHash: null,
+        context: context,
+      ),
+    );
+  }
+
+  @override
+  AutoDisposeProviderElement<bool> createElement() {
+    return _IsDarkModeProviderElement(this);
+  }
+
+  @override
+  bool operator ==(Object other) {
+    return other is IsDarkModeProvider && other.context == context;
+  }
+
+  @override
+  int get hashCode {
+    var hash = _SystemHash.combine(0, runtimeType.hashCode);
+    hash = _SystemHash.combine(hash, context.hashCode);
+
+    return _SystemHash.finish(hash);
+  }
+}
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+mixin IsDarkModeRef on AutoDisposeProviderRef<bool> {
+  /// The parameter `context` of this provider.
+  BuildContext get context;
+}
+
+class _IsDarkModeProviderElement extends AutoDisposeProviderElement<bool>
+    with IsDarkModeRef {
+  _IsDarkModeProviderElement(super.provider);
+
+  @override
+  BuildContext get context => (origin as IsDarkModeProvider).context;
+}
+
+String _$currentThemeOptionHash() =>
+    r'b1ac001b838385f992c49177a2713afa70c51012';
+
+/// See also [currentThemeOption].
+@ProviderFor(currentThemeOption)
+final currentThemeOptionProvider = AutoDisposeProvider<ThemeOption>.internal(
+  currentThemeOption,
+  name: r'currentThemeOptionProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$currentThemeOptionHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef CurrentThemeOptionRef = AutoDisposeProviderRef<ThemeOption>;
+String _$themeNotifierHash() => r'2c78ba9e8cfb661a8eec4a18f16579768732da4a';
+
+/// See also [ThemeNotifier].
+@ProviderFor(ThemeNotifier)
+final themeNotifierProvider =
+    AutoDisposeAsyncNotifierProvider<ThemeNotifier, ThemeOption>.internal(
+      ThemeNotifier.new,
+      name: r'themeNotifierProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$themeNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$ThemeNotifier = AutoDisposeAsyncNotifier<ThemeOption>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
